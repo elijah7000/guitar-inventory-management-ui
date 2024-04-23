a4f401176a4cc36f30948584b928ba3963ab404f
elijah7000 <143031360+Elijah-J-Kim@users.noreply.github.com>
2024-04-23 17:27:34 -0400

    jjkj

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode = 0;
 
 const host_local = "http://localhost:8080";
-const host_remote = "https://guitar-inventory-management-???.onrender.com";
+const host_remote = "https://test-3-latest.onrender.com";
 
 function getHost() {
   return mode == 0 ? host_local : host_remote;
@@ -14,46 +14,47 @@ function isLoggedIn() {
     return false;
   }
 }
-function getTheToken() {
-  return localStorage.getItem("token");
-}
-
-function saveTheToken(token) {
-  localStorage.setItem("token", token);
-  updateTheNavigationBar();
-}
-
-function removeTheToken() {
-  localStorage.removeItem("token");
-  updateTheNavigationBar();
-}
 
 let configuration = {
   isLoggedIn: () => isLoggedIn(),
   host: () => getHost(),
-  token: () => getTheToken(),
 };
 
-updateTheNavigationBar();
-
-async function updateTheNavigationBar() {
-  const navigation = document.getElementsByClassName("topnav")[0];
-  let loginTag = navigation.children[navigation.children.length - 1];
-  if (configuration.isLoggedIn()) {
-    loginTag.innerHTML = `<li class="right"><a href="#" onclick="logout()">Logout</a></li>`;
-  } else {
-    loginTag.innerHTML = `<li class="right><a href="login.html">Login</a></li>`;
+async function signup() {
+  let email = document.getElementById("email").value;
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let customer = { email: email, username: username, password: password };
+  let request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customer),
+  };
+  try {
+    let response = await fetch(getHost() + "/signup", request);
+    if (response.status == 200) {
+      alert("The registration was successful!");
+      location.href = "login.html";
+    } else {
+      console.log(`response status:${response.status}`);
+      alert("Something went wrong!");
+    }
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong!");
   }
 }
 
 async function login() {
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
+  let username = document.getElementById("usernameLogin").value;
+  let password = document.getElementById("passwordLogin").value;
   let customer = { username: username, password: password };
   let request = {
     method: "POST",
     headers: {
-      "Content-Type": "application/jspon",
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(customer),
   };
@@ -62,20 +63,16 @@ async function login() {
     if (response.status == 200) {
       alert("The login was successful!");
       const token = await response.text();
-      saveTheToken(token);
+
       location.href = "index.html";
     } else {
       console.log(`response status:${response.status}`);
-      removeTheToken();
+
       alert("Something went wrong!");
     }
   } catch (error) {
     console.log(error);
-    removeTheToken();
-    alert("Soemthign went wrong!");
-  }
-}
 
-async function logout() {
-  removeTheToken();
+    alert("Something went wrong!");
+  }
 }
